6a8db8c53bfbc7d2ed7bbe65d019c2e4bf4ac57f
Omkar1311 <93153698+Omkar1311@users.noreply.github.com>
2022-03-29 20:19:38 +0530

    Added file

---
+++ TNQ/Index.js
@@ -0,0 +1,24 @@
+const handleSubmit=()=>{
+    const data=  fetch('')
+    const res= data.JSON.parse(JSON.stringify())
+}
+
+
+const display=document.getElementById('displayData').innerHTML(
+    <div >
+        res.map(e)=>{
+            return(
+                <div key=''>
+                    
+
+
+                </div>
+
+
+            )
+           
+
+
+        }
+        </div>
+)
